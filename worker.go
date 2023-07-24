0126153b9e9b0e435eb2f8adbe9faf5b709351dc
MoeedDar <moeed.dar@gmail.com>
2023-07-24 08:45:49 +0100

    restart interface + baseten compatible prompting

--- worker.go
+++ worker.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -13,14 +14,19 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+type WorkerState int
+
 type WorkerConfig struct {
 	Host             string            `json:"host"`
 	Capacity         int               `json:"capacity"`
 	Heartbeat        int               `json:"heartbeat,omitempty"`
 	Timeout          int               `json:"timeout,omitempty"`
 	CheckAlive       bool              `json:"checkAlive,omitempty"`
+	MaxRetries       int               `json:"maxRetries,omitempty"`
+	Restart          bool              `json:"restart,omitempty"`
 	Headers          map[string]string `json:"headers,omitempty"`
 	GenerateEndpoint string            `json:"generateEndpoint,omitempty"`
+	WithBatch        bool              `json:"withBatch,omitempty"`
 }
 
 func (c *WorkerConfig) defaults() {
@@ -39,15 +45,16 @@ func (c *WorkerConfig) defaults() {
 }
 
 type WorkerStats struct {
-	Host      string
-	Alive     bool
-	Capacity  int
-	Queued    int
-	Running   int
-	Requests  int
-	Finished  int
-	Successes int
-	Fails     int
+	Host           string
+	Alive          bool
+	Capacity       int
+	Queued         int
+	Running        int
+	Requests       int
+	Finished       int
+	Successes      int
+	Fails          int
+	AvgRequestTime int
 }
 
 type Worker struct {
@@ -64,12 +71,20 @@ type Worker struct {
 	successes int32
 	failCount int32
 
+	maxRetries int
+	restart    bool
+
+	avgReqTime   int64
+	totalReqTime int64
+
 	heartbeat  time.Duration
 	timeout    time.Duration
 	checkAlive bool
 
 	headers          map[string]string
 	generateEndpoint string
+
+	withBatch bool
 }
 
 func NewWorker(config WorkerConfig) *Worker {
@@ -86,11 +101,14 @@ func NewWorker(config WorkerConfig) *Worker {
 		fails:            0,
 		successes:        0,
 		failCount:        0,
+		avgReqTime:       0,
+		totalReqTime:     0,
 		heartbeat:        time.Duration(config.Heartbeat) * time.Second,
 		timeout:          time.Duration(config.Timeout) * time.Second,
 		checkAlive:       config.CheckAlive,
 		headers:          config.Headers,
 		generateEndpoint: config.GenerateEndpoint,
+		withBatch:        config.WithBatch,
 	}
 }
 
@@ -118,20 +136,25 @@ func (w *Worker) Load() float64 {
 
 func (w *Worker) Stats() WorkerStats {
 	return WorkerStats{
-		Host:      w.host,
-		Capacity:  w.capacity,
-		Alive:     w.Alive,
-		Queued:    len(w.Jobs) + w.queue.Size(),
-		Running:   int(w.running),
-		Requests:  int(w.requests),
-		Finished:  int(w.finished),
-		Successes: int(w.successes),
-		Fails:     int(w.fails),
+		Host:           w.host,
+		Capacity:       w.capacity,
+		Alive:          w.Alive,
+		Queued:         len(w.Jobs) + w.queue.Size(),
+		Running:        int(w.running),
+		Requests:       int(w.requests),
+		Finished:       int(w.finished),
+		Successes:      int(w.successes),
+		Fails:          int(w.fails),
+		AvgRequestTime: int(w.avgReqTime),
 	}
 }
 
 func (w *Worker) doHearbeat() {
 	for range time.Tick(w.heartbeat) {
+		if !w.checkAlive {
+			return
+		}
+
 		alive := w.ping()
 		if w.Alive && !alive {
 			log.Error().Str("host", w.host).Msg("Worker has died")
@@ -161,6 +184,8 @@ func (w *Worker) generate(job *Job) {
 	failed := false
 	early := false
 
+	reqTime := time.Now().UnixMilli()
+
 	defer func() {
 		log.Info().Str("request id", job.Id).Str("worker host", w.host).Msg("Finishing generate request with worker")
 
@@ -178,9 +203,19 @@ func (w *Worker) generate(job *Job) {
 			w.countSuccess()
 		}
 
-		if w.failCount > 10 {
-			// TODO: reset
+		if w.failCount >= int32(w.maxRetries) {
+			w.checkAlive = false
+			w.Alive = false
+			if w.restart {
+				go w.doRestart()
+			}
 		}
+
+		if !w.Alive {
+			atomic.StoreInt32(&w.failCount, 0)
+		}
+
+		w.calcAvgReqTime(reqTime)
 	}()
 
 	select {
@@ -210,7 +245,20 @@ func (w *Worker) generate(job *Job) {
 			failed = true
 			return
 		}
-		token := string(data)
+
+		var token string
+		if w.withBatch {
+			jsn := make(map[string]string)
+			json.Unmarshal(data, &jsn)
+
+			var ok bool
+			token, ok = jsn["output"]
+			if !ok {
+				continue
+			}
+		} else {
+			token = string(data)
+		}
 
 		select {
 		case job.Output <- token:
@@ -251,7 +299,7 @@ func (w *Worker) prompt(ctx context.Context, payload []byte) (*http.Response, er
 	return http.DefaultClient.Do(req.WithContext(ctx))
 }
 
-func (w *Worker) restart() {
+func (w *Worker) doRestart() {
 
 }
 
@@ -263,4 +311,15 @@ func (w *Worker) countSuccess() {
 func (w *Worker) countFail() {
 	atomic.AddInt32(&w.fails, 1)
 	atomic.AddInt32(&w.failCount, 1)
+}
+
+func (w *Worker) calcAvgReqTime(reqTime int64) {
+	reqTime = time.Now().UnixMilli() - reqTime
+	atomic.AddInt64(&w.totalReqTime, reqTime)
+	atomic.AddInt32(&w.finished, 1)
+
+	totalReqTime := atomic.LoadInt64(&w.totalReqTime)
+	numRequests := atomic.LoadInt32(&w.finished)
+
+	w.avgReqTime = totalReqTime / int64(numRequests)
 }
